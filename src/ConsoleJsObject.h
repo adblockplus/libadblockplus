9df99efbcee43566dc7cd81ec58bac790649dff3
Felix Dahlke <felix@adblockplus.org>
2013-02-19 09:01:41 +0100

    Rename JsConsole to AdblockPlus::ConsoleJsObject

---
+++ src/ConsoleJsObject.h
@@ -0,0 +1,16 @@
+#ifndef ADBLOCK_PLUS_CONSOLE_JS_OBJECT_H
+#define ADBLOCK_PLUS_CONSOLE_JS_OBJECT_H
+
+#include <AdblockPlus/ErrorCallback.h>
+#include <v8.h>
+
+namespace AdblockPlus
+{
+  namespace ConsoleJsObject
+  {
+    v8::Handle<v8::ObjectTemplate> Create(
+      AdblockPlus::ErrorCallback& errorCallback);
+  }
+}
+
+#endif
