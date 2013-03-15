0e676e939f2f179305d8faaca6751aee6a4b8c82
shoniko <none@none>
2013-03-15 02:05:01 +0200

    Visual studio project generation script improvements

--- buildmsvs.bat
+++ buildmsvs.bat
@@ -1,4 +1,6 @@
 mkdir MSVS\shell
+mkdir build\Debug\obj\global_intermediate
+mkdir build\Release\obj\global_intermediate
 cd "third_party\v8\tools"
 python "js2c.py" "..\..\..\build\Debug\obj\global_intermediate\libraries.cc" "CORE" "off" "../src/runtime.js" "../src/v8natives.js" "../src/array.js" "../src/string.js" "../src/uri.js" "../src/math.js" "../src/messages.js" "../src/apinatives.js" "../src/debug-debugger.js" "../src/mirror-debugger.js" "../src/liveedit-debugger.js" "../src/date.js" "../src/json.js" "../src/regexp.js" "../src/macros.py"
 python "js2c.py" "..\..\..\build\Debug\obj\global_intermediate\experimental-libraries.cc" "EXPERIMENTAL" "off" "../src/runtime.js" "../src/v8natives.js" "../src/array.js" "../src/string.js" "../src/uri.js" "../src/math.js" "../src/messages.js" "../src/apinatives.js" "../src/debug-debugger.js" "../src/mirror-debugger.js" "../src/liveedit-debugger.js" "../src/date.js" "../src/json.js" "../src/regexp.js" "../src/macros.py"
