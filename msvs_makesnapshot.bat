846d95da84ebe441c403ae2046212f8e9f6e4503
shoniko <none@none>
2013-03-15 02:05:01 +0200

    Visual studio project generation script improvements

--- msvs_makesnapshot.bat
+++ msvs_makesnapshot.bat
@@ -1,1 +1,1 @@
-build\debug\mksnapshot.exe" "--log-snapshot-positions" "--logfile" "build\debug\obj\v8_snapshot\snapshot.log" "build\debug\obj\v8_snapshot\snapshot.cc"
+build\Debug\mksnapshot.exe --log-snapshot-positions --logfile "build\Debug\obj\v8_snapshot\snapshot.log" "build\Debug\obj\v8_snapshot\snapshot.cc"
