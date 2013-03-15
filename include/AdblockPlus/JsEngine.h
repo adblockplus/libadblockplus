8276d6e04a6c2c17badca8288945992e85ba7047
Wladimir Palant <trev@adblockplus.org>
2013-03-15 10:33:13 +0100

    Add file name and line number when reporting JavaScript exceptions

--- include/AdblockPlus/JsEngine.h
+++ include/AdblockPlus/JsEngine.h
@@ -13,7 +13,8 @@ namespace AdblockPlus
   class JsError : public std::runtime_error
   {
   public:
-    explicit JsError(const v8::Handle<v8::Value> exception);
+    explicit JsError(const v8::Handle<v8::Value> exception,
+        const v8::Handle<v8::Message> message);
   };
 
   class JsEngine
