e88e158617acb3ec468c801e818e77571e375e6d
Felix Dahlke <felix@adblockplus.org>
2013-04-04 04:51:43 +0200

    Move Mutex and Condition (now ConditionVariable) to the top level

--- src/Thread.h
+++ src/Thread.h
@@ -9,40 +9,40 @@
 
 namespace AdblockPlus
 {
+  class Mutex
+  {
+  public:
+#ifdef WIN32
+    CRITICAL_SECTION nativeMutex;
+#else
+    pthread_mutex_t nativeMutex;
+#endif
+
+    Mutex();
+    ~Mutex();
+    void Lock();
+    void Unlock();
+  };
+
+  class ConditionVariable
+  {
+  public:
+    ConditionVariable();
+    ~ConditionVariable();
+    void Wait(Mutex& mutex);
+    void Signal();
+
+  private:
+#ifdef WIN32
+    CONDITION_VARIABLE nativeCondition;
+#else
+    pthread_cond_t nativeCondition;
+#endif
+  };
+
   class Thread
   {
   public:
-    class Mutex
-    {
-    public:
-#ifdef WIN32
-      CRITICAL_SECTION nativeMutex;
-#else
-      pthread_mutex_t nativeMutex;
-#endif
-
-      Mutex();
-      ~Mutex();
-      void Lock();
-      void Unlock();
-    };
-
-    class Condition
-    {
-    public:
-      Condition();
-      ~Condition();
-      void Wait(Mutex& mutex);
-      void Signal();
-
-    private:
-#ifdef WIN32
-      CONDITION_VARIABLE nativeCondition;
-#else
-      pthread_cond_t nativeCondition;
-#endif
-    };
-
     virtual ~Thread();
     virtual void Run() = 0;
     void Start();
