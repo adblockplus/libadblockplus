7fbce3afdf02ecb64c339c56537ae4943bff0760
Wladimir Palant <trev@adblockplus.org>
2013-04-04 09:13:42 +0200

    Fixed indentation
    --HG--
    extra : rebase_source : fa70b5f3f16d65c145a1d8a727a3605af1c6b5bd

--- include/AdblockPlus/FilterEngine.h
+++ include/AdblockPlus/FilterEngine.h
@@ -6,34 +6,34 @@
 
 namespace AdblockPlus
 {
-    class JsEngine;
+  class JsEngine;
 
-    struct Subscription
-    {
-        std::string url;
-        std::string title;
+  struct Subscription
+  {
+    std::string url;
+    std::string title;
 
-        Subscription(const std::string& url, const std::string& title);
-    };
+    Subscription(const std::string& url, const std::string& title);
+  };
 
-    class FilterEngine
-    {
-    public:
-        explicit FilterEngine(JsEngine& jsEngine);
-        void AddSubscription(Subscription subscription);
-        void RemoveSubscription(const Subscription& subscription);
-        const Subscription* FindSubscription(const std::string& url) const;
-        const std::vector<Subscription>& GetSubscriptions() const;
-        void UpdateSubscriptionFilters(const Subscription& subscription);
-        std::vector<Subscription> FetchAvailableSubscriptions();
-        bool Matches(const std::string& url,
-                     const std::string& contentType) const;
-        std::vector<std::string> GetElementHidingRules() const;
+  class FilterEngine
+  {
+  public:
+    explicit FilterEngine(JsEngine& jsEngine);
+    void AddSubscription(Subscription subscription);
+    void RemoveSubscription(const Subscription& subscription);
+    const Subscription* FindSubscription(const std::string& url) const;
+    const std::vector<Subscription>& GetSubscriptions() const;
+    void UpdateSubscriptionFilters(const Subscription& subscription);
+    std::vector<Subscription> FetchAvailableSubscriptions();
+    bool Matches(const std::string& url,
+                 const std::string& contentType) const;
+    std::vector<std::string> GetElementHidingRules() const;
 
-    private:
-        JsEngine& jsEngine;
-        std::vector<Subscription> subscriptions;
-    };
+  private:
+    JsEngine& jsEngine;
+    std::vector<Subscription> subscriptions;
+  };
 }
 
 #endif
