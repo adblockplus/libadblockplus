75741e13485ce814ef251ea43fddad32d1173499
shoniko <none@none>
2013-04-10 16:35:17 +0300

    Visual Studio build instructions and changes.

--- buildmsvs.bat
+++ buildmsvs.bat
@@ -4,5 +4,6 @@ cd "third_party\v8\tools"
 python "js2c.py" "..\..\..\build\Debug\obj\global_intermediate\libraries.cc" "CORE" "off" "../src/runtime.js" "../src/v8natives.js" "../src/array.js" "../src/string.js" "../src/uri.js" "../src/math.js" "../src/messages.js" "../src/apinatives.js" "../src/debug-debugger.js" "../src/mirror-debugger.js" "../src/liveedit-debugger.js" "../src/date.js" "../src/json.js" "../src/regexp.js" "../src/macros.py"
 python "js2c.py" "..\..\..\build\Debug\obj\global_intermediate\experimental-libraries.cc" "EXPERIMENTAL" "off" "../src/runtime.js" "../src/v8natives.js" "../src/array.js" "../src/string.js" "../src/uri.js" "../src/math.js" "../src/messages.js" "../src/apinatives.js" "../src/debug-debugger.js" "../src/mirror-debugger.js" "../src/liveedit-debugger.js" "../src/date.js" "../src/json.js" "../src/regexp.js" "../src/macros.py"
 cd ..\..\..
-python convert_js.py include\jsSources.cpp
-third_party\gyp\gyp --depth=. -f msvs -G msvs_version=2010 --generator-output=MSVS -Dtarget_arch=ia32 -Dcomponent= -Dlibrary=static_library
+msbuild MSVS\libadblockplus.sln /m
+build\Debug\mksnapshot.exe --log-snapshot-positions --logfile "build\Debug\obj\v8_snapshot\snapshot.log" "build\Debug\obj\v8_snapshot\snapshot.cc"
+msbuild MSVS\libadblockplus.sln /m
