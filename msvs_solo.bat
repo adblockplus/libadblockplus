d0441281ce51945aa56d49694f27d888346896d3
eric <none@none>
2013-04-16 12:44:22 -0600

    Branch start. MSVS build with gyp.
    --HG--
    branch : msvs

---
+++ msvs_solo.bat
@@ -0,0 +1,24 @@
+@echo off
+setlocal
+pushd %~dp0
+set ARCH=x64     
+
+rem Verify that the python interpreter is available
+if not defined PYTHON (set PYTHON=python)
+%PYTHON% -c "exit 0" 2>nul
+if not errorlevel 9009 goto pythonOK
+echo Error. Cannot locate Python interpreter. 
+set spaces=    
+echo %spaces%PYTHON environment variable must point to a Python 2.7 interpreter.     
+echo %spaces%It may be either root name that appears in the path or the fully qualified name of an executable.
+echo %spaces%If the variable is absent, it defaults to "python".
+echo %spaces%PYTHON=%PYTHON%
+exit /b 1
+:pythonOK
+
+cls
+@echo on
+
+%PYTHON% third_party\gyp\gyp --help
+%PYTHON% third_party\gyp\gyp --depth=src -f msvs -I common.gypi --generator-output=build_solo -Dpython=%PYTHON% -Dtarget_arch=%ARCH% libadblockplus-solo.gyp
+echo Finished
