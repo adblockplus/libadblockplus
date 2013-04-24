a650a8a51bdbfc0c1dff80423a6ab5b9233a0cba
Wladimir Palant <trev@adblockplus.org>
2013-04-24 16:48:09 +0200

    Hardcode MSVS version, don't rely on GYP's probing

--- createsolution.bat
+++ createsolution.bat
@@ -5,7 +5,7 @@ if %1.==. goto NoArch
 pushd %~dp0
 mkdir build
 mkdir build\shell
-python msvs_gyp_wrapper.py --depth=. -f msvs -I common.gypi --generator-output=build -Dtarget_arch=%1 libadblockplus.gyp
+python msvs_gyp_wrapper.py --depth=. -f msvs -I common.gypi --generator-output=build -G msvs_version=2012 -Dtarget_arch=%1 libadblockplus.gyp
 goto End
 
 :NoArch
