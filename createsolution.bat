a16d6f12767f0236262ad9c98174c92f09eb3294
Wladimir Palant <trev@adblockplus.org>
2013-05-06 14:09:09 +0200

    MSVS: Separate ia32 and x64 build directories and make createsolution.bat create both sets of project files at once
    --HG--
    extra : rebase_source : d200ddcc2427a978225bf3790e9bd12b6b9c5740

--- createsolution.bat
+++ createsolution.bat
@@ -1,15 +1,7 @@
 @echo off
 
-if %1.==. goto NoArch
-
 pushd %~dp0
-mkdir build
-mkdir build\shell
-python msvs_gyp_wrapper.py --depth=. -f msvs -I common.gypi --generator-output=build -G msvs_version=2012 -Dtarget_arch=%1 libadblockplus.gyp
-goto End
-
-:NoArch
-  echo Please add a command line parameter specifying target architecture (ia32 or x64)
-  goto End
-
-:End
+mkdir build\ia32\shell
+mkdir build\x64\shell
+python msvs_gyp_wrapper.py --depth=build\ia32 -f msvs -I common.gypi --generator-output=build\ia32 -G msvs_version=2012 -Dtarget_arch=ia32 libadblockplus.gyp
+python msvs_gyp_wrapper.py --depth=build\x64 -f msvs -I common.gypi --generator-output=build\x64 -G msvs_version=2012 -Dtarget_arch=x64 libadblockplus.gyp
