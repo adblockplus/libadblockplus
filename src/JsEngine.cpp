01fd333f961bdb33cf2eb151319aac21086eb7a6
Alex Chronopoulos <achronop@gmail.com>
2020-11-24 11:15:56 +0100

    Noissue - Limit notification deadline to one second
    This looks like a regression from issue 6526 where `IdleNotification()`
    method replaced by `IdleNotificationDeadline()`. The former method's
    argument is in msec when the latter's method argument in seconds. The
    intention here must be to wait for one second.

--- src/JsEngine.cpp
+++ src/JsEngine.cpp
@@ -238,7 +238,7 @@ void AdblockPlus::JsEngine::TriggerEvent(const std::string& eventName,
 
 void AdblockPlus::JsEngine::Gc()
 {
-  while (!GetIsolate()->IdleNotificationDeadline(1000))
+  while (!GetIsolate()->IdleNotificationDeadline(1))
     ;
 }
 
